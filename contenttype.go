868e6027b6d66fc5ad225942f9ad52c5a4a9b881
RainInFall <onlycursedname@gmail.com>
2016-07-20 12:24:23 +0800

    add test

---
+++ contenttype.go
@@ -0,0 +1,170 @@
+package contenttype
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+//go:generate js-like object string string
+
+type ContentType struct {
+	typ        string
+	parameters Objectstringstring
+}
+
+func Parameters(a map[string]string) Objectstringstring {
+	return a
+}
+
+var paramRegExp, textRegExp, tokenRegExp *regexp.Regexp
+var qescRegExp, quoteRegExp, typeRegExp *regexp.Regexp
+
+/*
+Init should run before any functions of package called
+*/
+func Init() {
+	var err error
+
+	paramRegString := "; *([!#$%&'\\*\\+\\-\\.\\^_`\\|~0-9A-Za-z]+) *= *(\"(?:[\\x{000b}\\x{0020}\\x{0021}\\x{0023}-\\x{005b}\\x{005d}-\\x{007e}\\x{0080}-\\x{00ff}]|\\\\[\\x{000b}\\x{0020}-\\x{00ff}])*\"|[!#$%&'\\*\\+\\-\\.\\^_`\\|~0-9A-Za-z]+) *"
+	if paramRegExp, err = regexp.Compile(paramRegString); nil != err {
+		panic(err)
+	}
+
+	textRegString := "^[\\x{000b}\\x{0020}-\\x{007e}\\x{0080}-\\x{00ff}]+$"
+	if textRegExp, err = regexp.Compile(textRegString); nil != err {
+		panic(err)
+	}
+
+	tokenRegString := "^[!#$%&'\\*\\+\\-\\.\\^_`\\|~0-9A-Za-z]+$"
+	if tokenRegExp, err = regexp.Compile(tokenRegString); nil != err {
+		panic(err)
+	}
+
+	qescRegString := "\\\\([\\x{000b}\\x{0020}-\\x{00ff}])"
+	if qescRegExp, err = regexp.Compile(qescRegString); nil != err {
+		panic(err)
+	}
+
+	quoteRegString := "([\\\\\"])"
+	if quoteRegExp, err = regexp.Compile(quoteRegString); nil != err {
+		panic(err)
+	}
+
+	typeRegString := "^[!#$%&'\\*\\+\\-\\.\\^_`\\|~0-9A-Za-z]+\\/[!#$%&'\\*\\+\\-\\.\\^_`\\|~0-9A-Za-z]+$"
+	if typeRegExp, err = regexp.Compile(typeRegString); nil != err {
+		panic(err)
+	}
+}
+
+/*
+Format object to media type
+*/
+func Format(contentType *ContentType) (string, error) {
+	if !typeRegExp.MatchString(contentType.typ) {
+		return "", errors.New("invalid type")
+	}
+
+	result := contentType.typ
+
+	for _, param := range contentType.parameters.Keys().Sort() {
+		if !tokenRegExp.MatchString(param) {
+			return "", errors.New("invalid paremeter name")
+		}
+		if value, err := qstring(contentType.parameters[param]); nil == err {
+			result += "; " + param + "=" + value
+		} else {
+			return "", err
+		}
+	}
+
+	return result, nil
+}
+
+/*
+Parse
+*/
+
+func ParseRequest(req interface{}) (*ContentType, error) {
+	headerField := reflect.ValueOf(req).FieldByName("Header")
+	if !headerField.IsValid() || !headerField.Type().ConvertibleTo(reflect.TypeOf((http.Header)(nil))) {
+		return nil, errors.New("content-type header is missing from object")
+	}
+	return ParseHeader(headerField.Interface().(http.Header))
+}
+
+/*
+ParseHeader parse media type from http header
+*/
+func ParseHeader(header http.Header) (*ContentType, error) {
+	str := header.Get("content-type")
+	if "" == str {
+		return nil, errors.New("content-type header is missing from object")
+	}
+	return Parse(str)
+}
+
+/*
+Parse media type to object
+*/
+func Parse(str string) (*ContentType, error) {
+	index := strings.Index(str, ";")
+	var typ string
+	if -1 != index {
+		typ = strings.TrimSpace(str[0:index])
+	} else {
+		typ = strings.TrimSpace(str)
+	}
+
+	if !typeRegExp.MatchString(typ) {
+		return nil, errors.New("invalid media type")
+	}
+
+	typ = strings.ToLower(typ)
+	parameters := make(map[string]string)
+
+	for _, match := range paramRegExp.FindAllStringSubmatchIndex(str, -1) {
+		if match[0] != index {
+			return nil, errors.New("invalid parameter format")
+		}
+
+		index = match[1]
+		key := strings.ToLower(str[match[2]:match[3]])
+		value := str[match[4]:match[5]]
+
+		if '"' == value[0] {
+			value = qescRegExp.ReplaceAllStringFunc(
+				value[1:len(value)-1],
+				func(match string) string {
+					return qescRegExp.FindStringSubmatch(match)[1]
+				})
+		}
+
+		parameters[key] = value
+	}
+
+	if -1 != index && len(str) != index {
+		return nil, errors.New("invalid parameter format")
+	}
+
+	return &ContentType{
+		typ,
+		parameters,
+	}, nil
+}
+
+func qstring(str string) (string, error) {
+	if tokenRegExp.MatchString(str) {
+		return str, nil
+	}
+
+	if len(str) > 0 && !textRegExp.MatchString(str) {
+		return str, errors.New("invalid parameter value")
+	}
+
+	return "\"" + quoteRegExp.ReplaceAllStringFunc(str, func(match string) string {
+		return "\\" + quoteRegExp.FindStringSubmatch(match)[1]
+	}) + "\"", nil
+}
---
+++ parse_test.go
@@ -0,0 +1,151 @@
+package contenttype
+
+import (
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/RainInFall/assert"
+)
+
+var invalidTypes = []string{
+	" ",
+	"null",
+	"undefined",
+	"/",
+	"text / plain",
+	"text/;plain",
+	"text/\"plain",
+	"text/p£ain",
+	"text/(plain)",
+	"text/@plain",
+	"text/plain,wrong",
+}
+
+func TestParse(t *testing.T) {
+	assert.Init(t)
+	Init()
+
+	func() {
+		typ, err := Parse("text/html")
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+	}()
+
+	func() {
+		typ, err := Parse("image/svg+xml")
+		assert.Ok(nil == err)
+		assert.Ok("image/svg+xml" == typ.typ)
+	}()
+
+	func() {
+		typ, err := Parse(" text/html")
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+	}()
+
+	func() {
+		typ, err := Parse("text/html; charset=utf-8; foo=bar")
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+		assert.Ok(typ.parameters.Equals(Parameters(map[string]string{
+			"charset": "utf-8",
+			"foo":     "bar",
+		})))
+	}()
+
+	func() {
+		typ, err := Parse("text/html ; charset=utf-8 ; foo=bar")
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+		assert.Ok(typ.parameters.Equals(Parameters(map[string]string{
+			"charset": "utf-8",
+			"foo":     "bar",
+		})))
+	}()
+
+	func() {
+		typ, err := Parse("IMAGE/SVG+XML")
+		assert.Ok(nil == err)
+		assert.Ok("image/svg+xml" == typ.typ)
+	}()
+
+	func() {
+		typ, err := Parse("text/html; Charset=UTF-8")
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+		assert.Ok(typ.parameters.Equals(Parameters(map[string]string{
+			"charset": "UTF-8",
+		})))
+	}()
+
+	func() {
+		typ, err := Parse("text/html; charset=\"UTF-8\"")
+		assert.Ok(err == nil)
+		assert.Ok(typ.parameters.Equals(Parameters(map[string]string{
+			"charset": "UTF-8",
+		})))
+	}()
+
+	func() {
+		typ, err := Parse("text/html; charset = \"UT\\F-\\\\\\\"8\\\"\"")
+		assert.Ok(nil == err)
+		assert.Ok(typ.parameters.Equals(Parameters(map[string]string{
+			"charset": "UTF-\\\"8\"",
+		})))
+	}()
+
+	func() {
+		typ, err := Parse("text/html; param=\"charset=\\\"utf-8\\\"; foo=bar\"; bar=foo")
+		assert.Ok(nil == err)
+		assert.Ok(typ.parameters.Equals(Parameters(map[string]string{
+			"param": "charset=\"utf-8\"; foo=bar",
+			"bar":   "foo",
+		})))
+	}()
+
+	for _, v := range invalidTypes {
+		_, err := Parse(v)
+		assert.Ok(nil != err && regexp.MustCompile("invalid media type").MatchString(err.Error()))
+	}
+
+	func() {
+		_, err := Parse("text/plain; foo=\"bar")
+		assert.Ok(nil != err && regexp.MustCompile("invalid parameter format").MatchString(err.Error()))
+	}()
+	func() {
+		_, err := Parse("text/plain; profile=http://localhost; foo=bar")
+		assert.Ok(nil != err && regexp.MustCompile("invalid parameter format").MatchString(err.Error()))
+	}()
+	func() {
+		_, err := Parse("text/plain; profile=http://localhost")
+		assert.Ok(nil != err && regexp.MustCompile("invalid parameter format").MatchString(err.Error()))
+	}()
+
+	func() {
+		header := make(http.Header)
+		header.Set("content-type", "text/html")
+		typ, err := ParseHeader(header)
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+	}()
+
+	func() {
+		req := http.Request{
+			Header: http.Header{},
+		}
+		_, err := ParseRequest(req)
+		assert.Ok(nil != err && regexp.MustCompile("content-type header is missing").MatchString(err.Error()))
+	}()
+
+	func() {
+		req := http.Request{
+			Header: http.Header{
+				"Content-Type": []string{"text/html"},
+			},
+		}
+		typ, err := ParseRequest(req)
+		assert.Ok(nil == err)
+		assert.Ok("text/html" == typ.typ)
+	}()
+}
